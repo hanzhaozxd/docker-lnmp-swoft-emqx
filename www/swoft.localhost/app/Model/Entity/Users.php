68939cab344c86a7a4805ae8740f3ea04e64c395
hanzhao <yy18335385210@163.com>
2019-09-19 12:29:45 +0800

    devtool 自动生成的实体类

---
+++ www/swoft.localhost/app/Model/Entity/Users.php
@@ -0,0 +1,212 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Model\Entity;
+
+use Swoft\Db\Annotation\Mapping\Column;
+use Swoft\Db\Annotation\Mapping\Entity;
+use Swoft\Db\Annotation\Mapping\Id;
+use Swoft\Db\Eloquent\Model;
+
+
+/**
+ * 
+ * Class Users
+ *
+ * @since 2.0
+ *
+ * @Entity(table="users")
+ */
+class Users extends Model
+{
+    /**
+     * 
+     * @Id()
+     * @Column()
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * 
+     *
+     * @Column()
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * 昵称
+     *
+     * @Column()
+     *
+     * @var string|null
+     */
+    private $nickname;
+
+    /**
+     * 
+     *
+     * @Column()
+     *
+     * @var string|null
+     */
+    private $email;
+
+    /**
+     * 
+     *
+     * @Column(hidden=true)
+     *
+     * @var string
+     */
+    private $password;
+
+    /**
+     * 
+     *
+     * @Column(name="created_at", prop="createdAt")
+     *
+     * @var string|null
+     */
+    private $createdAt;
+
+    /**
+     * 
+     *
+     * @Column(name="updated_at", prop="updatedAt")
+     *
+     * @var string|null
+     */
+    private $updatedAt;
+
+
+    /**
+     * @param int $id
+     *
+     * @return void
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string|null $nickname
+     *
+     * @return void
+     */
+    public function setNickname(?string $nickname): void
+    {
+        $this->nickname = $nickname;
+    }
+
+    /**
+     * @param string|null $email
+     *
+     * @return void
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return void
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param string|null $createdAt
+     *
+     * @return void
+     */
+    public function setCreatedAt(?string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @param string|null $updatedAt
+     *
+     * @return void
+     */
+    public function setUpdatedAt(?string $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCreatedAt(): ?string
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUpdatedAt(): ?string
+    {
+        return $this->updatedAt;
+    }
+
+}
